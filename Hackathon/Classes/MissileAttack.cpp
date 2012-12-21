ce4ac47e8c9cc23aea718d87e6448400d39e9cdb
강 건우 <code.athei@gmail.com>
2012-12-22 04:06:55 +0900

    to pull

--- Hackathon/Classes/MissileAttack.cpp
+++ Hackathon/Classes/MissileAttack.cpp
@@ -7,3 +7,58 @@
 //
 
 #include "MissileAttack.h"
+
+bool MissileAttack::init()
+{
+    if (!Attack::init())
+        return false;
+    
+    isHit = false;
+    
+    angle = 0;
+    distance = 400;
+    
+    resetPosition();
+    scheduleUpdate();
+    
+    return true;
+}
+
+void MissileAttack::resetPosition()
+{
+    CCPoint p(0, distance);
+    
+    CCAffineTransform transform = CCAffineTransformRotate(CCAffineTransformIdentity, angle);
+    p = CCPointApplyAffineTransform(p, transform);
+    missile->setRotation(-angle / M_PI * 180);
+    missile->setPosition(p);
+    
+    p = CCPoint(0, 120);
+    
+    transform = CCAffineTransformRotate(CCAffineTransformIdentity, angle);
+    p = CCPointApplyAffineTransform(p, transform);
+    aim->setRotation(-angle / M_PI * 180);
+    aim->setPosition(p);
+}
+
+void MissileAttack::update(float dt)
+{
+    distance -= dt * 800;
+    
+    resetPosition();
+}
+
+bool MissileAttack::enable()
+{
+    if (distance < 120)
+        return false;
+    
+    return true;
+}
+
+void MissileAttack::rotate(float r)
+{
+    angle += r;
+    
+    resetPosition();
+}
